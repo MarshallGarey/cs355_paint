67f4b02a9fa25cebe26df53318fb7f76c19523b2
MarshallGarey <gaomarshall9@gmail.com>
2017-04-18 16:36:17 -0600

    Median and uniform blur also implemented. That leaves just sharpening and edge detection. Also, I need to make zooming/scrolling work with images.

--- src/cs355/model/drawing/Image.java
+++ src/cs355/model/drawing/Image.java
@@ -4,14 +4,19 @@ import cs355.model.image.CS355Image;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 /**
  * Created by Marshall Garey
  */
 public class Image extends CS355Image {
 
-    public Image() {
+    public Image () {
         super();
+    }
+
+    public Image(int width, int height) {
+        super(width, height);
     }
 
     // Convert to a buffered image
@@ -52,12 +57,144 @@ public class Image extends CS355Image {
 
     @Override
     public void medianBlur() {
+        // Temporary image to write data to.
+        Image newImage = new Image(getWidth(), getHeight());
+        int[] rgb = new int[3];
+        int width = getWidth();
+        int height = getHeight();
 
+        // For each pixel:
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+
+                // Get the color from the image.
+                getPixel(x, y, rgb);
+
+                // Copy the border pixels to the new image, but don't filter them.
+                // We need to copy or else the image will have a blank border.
+                // So, if the pixel is not a border pixel, blur it.
+                if (!(x == 0 || y == 0 || x == width-1 || y == height-1)) {
+
+                    // Blur the pixel
+                    medianBlurPixel(rgb, x, y);
+                }
+
+                // Set the pixel in the new image to preserve old data.
+                newImage.setPixel(x, y, rgb);
+            }
+        }
+
+        // Update this image.
+        this.setPixels(newImage);
     }
 
     @Override
     public void uniformBlur() {
+        // Temporary image to write data to.
+        Image newImage = new Image(getWidth(), getHeight());
+        int[] rgb = new int[3];
+        int width = getWidth();
+        int height = getHeight();
 
+        // For each pixel:
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+
+                // Get the color from the image.
+                getPixel(x, y, rgb);
+
+                // Copy the border pixels to the new image, but don't filter them.
+                // We need to copy or else the image will have a blank border.
+                // So, if the pixel is not a border pixel, blur it.
+                if (!(x == 0 || y == 0 || x == width-1 || y == height-1)) {
+
+                    // Blur the pixel
+                    blurPixel(rgb, x, y);
+                }
+
+                // Set the pixel in the new image to preserve old data.
+                newImage.setPixel(x, y, rgb);
+            }
+        }
+
+        // Update this image.
+        this.setPixels(newImage);
+    }
+
+    /**
+     * Performs a pixel blur
+     * Can potentially crash from indexing out of bounds, so I need to make
+     * sure I don't index out of bounds
+     *
+     * @param pixel Return value
+     * @param x X coordinate of the pixel
+     * @param y Y coordinate of the pixel
+     */
+    private void blurPixel(int[] pixel, int x, int y) {
+
+        int redTotal;
+        int greenTotal;
+        int blueTotal;
+        redTotal = greenTotal = blueTotal = 0;
+        int[] neighbor = new int[3];
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+
+                // Get the neighboring pixel
+                getPixel(x + i, y + j, neighbor);
+
+                // Add the red, green, and blue values to totals
+                redTotal += neighbor[0];
+                greenTotal += neighbor[1];
+                blueTotal += neighbor[2];
+
+            }
+        }
+
+        pixel[0] = redTotal / 9;
+        pixel[1] = greenTotal / 9;
+        pixel[2] = blueTotal / 9;
+    }
+
+    /**
+     * Performs a median pixel blur
+     * Can potentially crash from indexing out of bounds, so I need to make
+     * sure I don't index out of bounds
+     *
+     * @param pixel Return value
+     * @param x X coordinate of the pixel
+     * @param y Y coordinate of the pixel
+     */
+    private void medianBlurPixel(int[] pixel, int x, int y) {
+
+        ArrayList<Integer> reds = new ArrayList<>();
+        ArrayList<Integer> greens = new ArrayList<>();
+        ArrayList<Integer> blues = new ArrayList<>();
+        int[] neighbor = new int[3];
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+
+                // Get the neighboring pixel
+                getPixel(x + i, y + j, neighbor);
+
+                // Add the red, green, and blue values to totals
+                reds.add(neighbor[0]);
+                greens.add(neighbor[1]);
+                blues.add(neighbor[2]);
+
+            }
+        }
+        // Sort
+        java.util.Collections.sort(reds);
+        java.util.Collections.sort(greens);
+        java.util.Collections.sort(blues);
+
+        // Get middle
+        pixel[0] = reds.get(5);
+        pixel[1] = greens.get(5);
+        pixel[2] = blues.get(5);
     }
 
     // Convert the image to HSB, zero the saturation channel, and convert back to RGB.
@@ -90,7 +227,6 @@ public class Image extends CS355Image {
         }
     }
 
-    // TODO: contrast isn't working correctly
     // Range [-100,100], where 100 is maximum contrast (8x) and -100 is zero contrast
     // and 0 is no change.
     // With input brightness in the range [0,1] and amount as the contrast parameter:
