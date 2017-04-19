025d5d3c75ac5a440fd0213ade4c109ff399bc4f
MarshallGarey <gaomarshall9@gmail.com>
2017-04-18 20:54:20 -0600

    Median filter has a problem. Sharpening implemented

--- src/cs355/model/drawing/Image.java
+++ src/cs355/model/drawing/Image.java
@@ -52,7 +52,84 @@ public class Image extends CS355Image {
 
     @Override
     public void sharpen() {
+        // Temporary image to write data to.
+        Image newImage = new Image(getWidth(), getHeight());
+        int[] rgb = new int[3];
+        int width = getWidth();
+        int height = getHeight();
 
+        // For each pixel:
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+
+                // Get the color from the image.
+                getPixel(x, y, rgb);
+
+                // Copy the border pixels to the new image, but don't filter them.
+                // We need to copy or else the image will have a blank border.
+                // So, if the pixel is not a border pixel, blur it.
+                if (!(x == 0 || y == 0 || x == width-1 || y == height-1)) {
+
+                    // Blur the pixel
+                    sharpenPixel(rgb, x, y);
+                }
+
+                // Set the pixel in the new image to preserve old data.
+                newImage.setPixel(x, y, rgb);
+            }
+        }
+
+        // Update this image.
+        this.setPixels(newImage);
+    }
+
+    private void sharpenPixel(int[] pixel, int x, int y) {
+        int redTotal;
+        int greenTotal;
+        int blueTotal;
+        redTotal = greenTotal = blueTotal = 0;
+        int[] neighbor = new int[3];
+
+
+        // Get the neighboring pixel
+        // Subtract the red, green, and blue neighbors
+        // left neighbor
+        getPixel(x - 1, y, neighbor);
+        redTotal -= neighbor[0];
+        greenTotal -= neighbor[1];
+        blueTotal -= neighbor[2];
+        // right neighbor
+        getPixel(x + 1, y, neighbor);
+        redTotal -= neighbor[0];
+        greenTotal -= neighbor[1];
+        blueTotal -= neighbor[2];
+        // top neighbor
+        getPixel(x, y - 1, neighbor);
+        redTotal -= neighbor[0];
+        greenTotal -= neighbor[1];
+        blueTotal -= neighbor[2];
+        // bottom neighbor
+        getPixel(x, y + 1, neighbor);
+        redTotal -= neighbor[0];
+        greenTotal -= neighbor[1];
+        blueTotal -= neighbor[2];
+
+        // Add 6 times the middle
+        getPixel(x, y, neighbor);
+        redTotal += 6*neighbor[0];
+        greenTotal += 6*neighbor[1];
+        blueTotal += 6*neighbor[2];
+
+        // Divide by 2
+        pixel[0] = redTotal / 2;
+        pixel[1] = greenTotal / 2;
+        pixel[2] = blueTotal / 2;
+
+        // Clip to the range [0,255]
+        for (int i = 0; i < 3; i++) {
+            if (pixel[i] < 0) pixel[i] = 0;
+            else if (pixel[i] > 255) pixel[i] = 255;
+        }
     }
 
     @Override
@@ -122,7 +199,7 @@ public class Image extends CS355Image {
     }
 
     /**
-     * Performs a pixel blur
+     * Performs a uniform pixel blur using 8 surrounding neighbors.
      * Can potentially crash from indexing out of bounds, so I need to make
      * sure I don't index out of bounds
      *
@@ -158,7 +235,8 @@ public class Image extends CS355Image {
     }
 
     /**
-     * Performs a median pixel blur
+     * TODO: THIS IS NOT FINISHED
+     * Performs a median pixel blur using 8 surrounding neighbors.
      * Can potentially crash from indexing out of bounds, so I need to make
      * sure I don't index out of bounds
      *
@@ -179,7 +257,7 @@ public class Image extends CS355Image {
                 // Get the neighboring pixel
                 getPixel(x + i, y + j, neighbor);
 
-                // Add the red, green, and blue values to totals
+                // Add the red, green, and blue values to lists.
                 reds.add(neighbor[0]);
                 greens.add(neighbor[1]);
                 blues.add(neighbor[2]);
@@ -191,10 +269,55 @@ public class Image extends CS355Image {
         java.util.Collections.sort(greens);
         java.util.Collections.sort(blues);
 
-        // Get middle
-        pixel[0] = reds.get(5);
-        pixel[1] = greens.get(5);
-        pixel[2] = blues.get(5);
+        // Get the median color
+        int medianColor[] = new int[3];
+        medianColor[0] = reds.get(5);
+        medianColor[1] = greens.get(5);
+        medianColor[2] = blues.get(5);
+
+        // Find which neighboring pixel is "closest" to the median color
+        // by treating the colors as vectors and computing the
+        // squared distance between them and using the least distance.
+        // Squared distance is (r2-r1)^2 + (g2-g1)^2 + (b2-b1)^2
+
+        int closestPixel[] = neighbor;
+
+        // Make the least distance negative so it will be updated on the first iteration.
+        double least = -1;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+
+                // Get the neighboring pixel
+                getPixel(x + i, y + j, neighbor);
+
+                // Compute squared distance
+                double dist = squaredDistance(neighbor, medianColor);
+
+                // Update the lowest distance and closest pixel.
+                if ((least < 0) || (dist < least)) {
+                    least = dist;
+                    closestPixel = neighbor;
+                }
+            }
+        }
+
+        pixel[0] = closestPixel[0];
+        pixel[1] = closestPixel[1];
+        pixel[2] = closestPixel[2];
+    }
+
+    /**
+     * Compute the squared distance between 2 pixels:
+     *   (r2-r1)^2 + (g2-g1)^2 + (b2-b1)^2
+     * @param p1 First pixel
+     * @param p2 Second pixel
+     * @return Squared distance between them.
+     */
+    private double squaredDistance(int[] p1, int[] p2) {
+        double r = (p2[0] - p1[0]);
+        double g = (p2[1] - p1[1]);
+        double b = (p2[2] - p1[2]);
+        return r*r + g*g + b*b;
     }
 
     // Convert the image to HSB, zero the saturation channel, and convert back to RGB.
